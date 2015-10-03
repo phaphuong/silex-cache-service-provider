6eb95ec8940e0279cb761457848bd3dc98e9b38b
phaphuong <phaphuong@gmail.com>
2015-10-03 21:53:39 +0300

    updated to silex 2

--- src/Moust/Silex/Provider/CacheServiceProvider.php
+++ src/Moust/Silex/Provider/CacheServiceProvider.php
@@ -11,13 +11,14 @@
 
 namespace Moust\Silex\Provider;
 
+use Pimple\Container;
 use Silex\Application;
-use Silex\ServiceProviderInterface;
+use Silex\Api\ServiceProviderInterface;
 use Moust\Silex\Cache\CacheFactory;
 
 class CacheServiceProvider implements ServiceProviderInterface
 {
-    public function register(Application $app)
+    public function register(Container $app)
     {
         $app['cache.default_options'] = array(
             'driver' => 'array'
@@ -25,20 +26,20 @@ class CacheServiceProvider implements ServiceProviderInterface
 
         $app['cache.drivers'] = function () {
             return array(
-                'apc'       => '\\Moust\\Silex\\Cache\\ApcCache',
-                'array'     => '\\Moust\\Silex\\Cache\\ArrayCache',
-                'file'      => '\\Moust\\Silex\\Cache\\FileCache',
-                'memcache'  => '\\Moust\\Silex\\Cache\\MemcacheCache',
+                'apc' => '\\Moust\\Silex\\Cache\\ApcCache',
+                'array' => '\\Moust\\Silex\\Cache\\ArrayCache',
+                'file' => '\\Moust\\Silex\\Cache\\FileCache',
+                'memcache' => '\\Moust\\Silex\\Cache\\MemcacheCache',
                 'memcached' => '\\Moust\\Silex\\Cache\\MemcachedCache',
-                'xcache'    => '\\Moust\\Silex\\Cache\\XcacheCache',
-                'redis'     => '\\Moust\\Silex\\Cache\\RedisCache',
-                'wincache'  => '\\Moust\\Silex\\Cache\\WincacheCache',
+                'xcache' => '\\Moust\\Silex\\Cache\\XcacheCache',
+                'redis' => '\\Moust\\Silex\\Cache\\RedisCache',
+                'wincache' => '\\Moust\\Silex\\Cache\\WincacheCache',
             );
         };
 
-        $app['cache.factory'] = $app->share(function ($app) {
+        $app['cache.factory'] = function ($app) {
             return new CacheFactory($app['cache.drivers'], $app['caches.options']);
-        });
+        };
 
         $app['caches.options.initializer'] = $app->protect(function () use ($app) {
             static $initialized = false;
@@ -64,7 +65,7 @@ class CacheServiceProvider implements ServiceProviderInterface
             $app['caches.options'] = $tmp;
         });
 
-        $app['caches'] = $app->share(function ($app) {
+        $app['caches'] = function ($app) {
             $app['caches.options.initializer']();
 
             $caches = new \Pimple();
@@ -82,9 +83,9 @@ class CacheServiceProvider implements ServiceProviderInterface
             }
 
             return $caches;
-        });
+        };
 
-        $app['caches.config'] = $app->share(function ($app) {
+        $app['caches.config'] = function ($app) {
             $app['caches.options.initializer']();
 
             $configs = new \Pimple();
@@ -93,24 +94,19 @@ class CacheServiceProvider implements ServiceProviderInterface
             }
 
             return $configs;
-        });
+        };
 
         // shortcuts for the "first" cache
-        $app['cache'] = $app->share(function ($app) {
+        $app['cache'] = function ($app) {
             $caches = $app['caches'];
 
             return $caches[$app['caches.default']];
-        });
+        };
 
-        $app['cache.config'] = $app->share(function ($app) {
+        $app['cache.config'] = function ($app) {
             $caches = $app['caches.config'];
 
             return $caches[$app['caches.default']];
-        });
-    }
-
-    public function boot(Application $app)
-    {
-
+        };
     }
 }
